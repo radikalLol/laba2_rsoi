c1520f3296d7cfdc645538e461f552e499b910a0
radik <radikal.xxxxx@gmail.com>
2020-02-21 03:47:21 +0300

    e
    e

--- src/config.py
+++ src/config.py
@@ -9,17 +9,18 @@ class Development(object):
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY') or 'hhgaghhgsdhdhdd'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgresql://User01:777@localhost/food_delivery'
 class Production(object):
     """
     Production environment configurations
     """
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'hhgaghhgsdhdhdd'
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgresql://User01:777@localhost/food_delivery'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY') or 'hhgaghhgsdhdhdd'
 
 app_config = {
     'development': Development,
     'production': Production,
 }
+
